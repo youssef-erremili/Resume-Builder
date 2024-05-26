9bbf1b27a8e134834781d8b996808201eedcc8fb
youssef-erremili <yousseferremili19@gmail.com>
2024-05-26 21:24:44 +0100

    Setup config with database

--- config/config.php
+++ config/config.php
@@ -1,9 +1,20 @@
-<?php 
-    $server = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "errehubdb";
-    $conToserver = new mysqli($server, $username, $password, $dbname);
-    if($conToserver->connect_error){
-        die("could not connect to the server". $conToserver->connect_error);
+<?php
+$host = "localhost";
+$admin = "root";
+$password = "";
+$databaseName = "resume-builder";
+
+try {
+    $serverConnection = new PDO("mysql:host = $host; dbname=$databaseName", $admin, $password);
+    $serverConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    if ($serverConnection) {
+        echo "<script> 
+                console.log(\"Connection Has Been Established Successfully\")
+            </script>";
     }
+} catch (PDOException $th) {
+    echo "connection failed" . $th->getMessage();
+    echo "<script> 
+            console.error(\"Connection Has Not Been Established Successfully\")
+        </script>";
+}
