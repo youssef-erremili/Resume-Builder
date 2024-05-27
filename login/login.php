a5e62d86ae2cbe42a1d293cb8fcb7804bbefffc5
Youssef Erremili <yousseferremili19@gmail.com>
2024-05-27 16:24:06 +0200

    Setup login page

--- login/login.php
+++ login/login.php
@@ -1,3 +1,33 @@
+<?php
+session_start();
+include("../config/config.php");
+
+$alertMsg = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["login"])) {
+        $email_address = trim(htmlspecialchars($_POST["EmailAddress"]));
+        $password =  trim(htmlspecialchars($_POST["password"]));
+        $firstQuery = "SELECT * FROM users WHERE email_address = :email_address LIMIT 1";
+        $stmt = $serverConnection->prepare($firstQuery);
+        $stmt->bindParam(":email_address", $email_address, PDO::PARAM_STR);
+        $stmt->execute();
+        if ($stmt->rowCount() > 0) {
+            $user_password = $stmt->fetch(PDO::FETCH_ASSOC);
+            if (password_verify($password, $user_password["user_password"])) {
+                $userName = $user_password['full_name'];
+                $_SESSION["username"] = $userName;
+                $_SESSION["loggedin"] = true;
+                header("Location: ../editor/editor.php");
+            } else {
+                $alertMsg = "Invalid password";
+            }
+        } else {
+            $alertMsg = "Invalid email";
+        }
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -26,7 +56,7 @@
                     <h2>Log In</h2>
                     <p>Welcome back!</p>
                 </div>
-                <form action="" method="post" name="form" class="submitForm">
+                <form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="POST" name="form" class="submitForm">
                     <div class="second sectionYU">
                         <input type="email" name="EmailAddress" spellcheck="false" id="EmailAddress" autocomplete="true" placeholder="Email Address">
                         <ion-icon name="mail-outline"></ion-icon>
@@ -36,12 +66,12 @@
                         <img src="../assests/image/show.svg" alt="show-password" class="password" id="passIcon">
                         <ion-icon name="lock-closed-outline"></ion-icon>
                     </div>
-                    <input type="submit" id="submit" value="login in">
-                    <p class="havAccc">Don't have an account? <a href="../sign-up/sign-in.html">Sign in</a></p>
+                    <input type="submit" id="submit" name="login" value="log in">
+                    <p class="havAccc">Don't have an account? <a href="../signup/sign-up.php">Sign up</a></p>
                 </form>
                 <div class="socialMedia">
                     <ul>
-                        <a target="_blank" href="https://github.com/Youssef19er"><ion-icon name="logo-github"></ion-icon></a>
+                        <a target="_blank" href="https://github.com/youssef-erremili"><ion-icon name="logo-github"></ion-icon></a>
                         <a target="_blank" href="https://www.linkedin.com/in/youssef-erremili-100070296/"><ion-icon name="logo-linkedin"></ion-icon></a>
                         <a target="_blank" href="https://www.instagram.com/youssef.erremili.990/"><ion-icon name="logo-instagram"></ion-icon></a>
                     </ul>
@@ -49,8 +79,26 @@
             </div>
         </div>
     </main>
+    <script>
+        document.addEventListener('DOMContentLoaded', (event) => {
+            let alertMsg = "<?php echo $alertMsg; ?>";
+            const notyf = new Notyf();
+            if (alertMsg == "Invalid password") {
+                notyf.error({
+                    message: 'Password is uncorrect',
+                    duration: 6000,
+                });
+            } else if (alertMsg == "Invalid email") {
+                notyf.error({
+                    message: 'Email Address is uncorrect',
+                    duration: 6000,
+                });
+            }
+        });
+    </script>
     <script src="../assests/js/login.js"></script>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.js"></script>
 </body>
+
 </html>
