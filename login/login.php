ec33d582b685197b2785d9c3ba47a30c040582a2
Youssef Erremili <yousseferremili19@gmail.com>
2024-05-27 14:04:56 +0200

    fix bug in login page

---
+++ login/login.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login - errehub</title>
+    <link rel="stylesheet" href="../assests/styles/register.css">
+    <link rel="shortcut icon" href="../assests/image/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.css">
+</head>
+<body>
+    <main class="main">
+        <div class="left">
+            <div class="logo">
+                <a href="../index.html">
+                    <img src="../assests/image/errehub-dark-font.webp" alt="errehub website logo">
+                </a>
+            </div>
+            <div class="head">
+                <h1>Welcome Home! Where Your Journey Begins with Every Login.</h1>
+            </div>
+        </div>
+        <div class="right">
+            <div class="form loginForm">
+                <div class="greetings">
+                    <h2>Log In</h2>
+                    <p>Welcome back!</p>
+                </div>
+                <form action="" method="post" name="form" class="submitForm">
+                    <div class="second sectionYU">
+                        <input type="email" name="EmailAddress" spellcheck="false" id="EmailAddress" autocomplete="true" placeholder="Email Address">
+                        <ion-icon name="mail-outline"></ion-icon>
+                    </div>
+                    <div class="third sectionYU">
+                        <input type="password" name="password" id="Password1" spellcheck="false" class="inputField" autocomplete="true" placeholder="Password">
+                        <img src="../assests/image/show.svg" alt="show-password" class="password" id="passIcon">
+                        <ion-icon name="lock-closed-outline"></ion-icon>
+                    </div>
+                    <input type="submit" id="submit" value="login in">
+                    <p class="havAccc">Don't have an account? <a href="../sign-up/sign-in.html">Sign in</a></p>
+                </form>
+                <div class="socialMedia">
+                    <ul>
+                        <a target="_blank" href="https://github.com/Youssef19er"><ion-icon name="logo-github"></ion-icon></a>
+                        <a target="_blank" href="https://www.linkedin.com/in/youssef-erremili-100070296/"><ion-icon name="logo-linkedin"></ion-icon></a>
+                        <a target="_blank" href="https://www.instagram.com/youssef.erremili.990/"><ion-icon name="logo-instagram"></ion-icon></a>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </main>
+    <script src="../assests/js/login.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.js"></script>
+</body>
+</html>
