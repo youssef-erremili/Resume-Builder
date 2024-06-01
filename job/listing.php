e6fcf8e6a8a8b5fa0c6f2c28cb06c4e65e454a2f
youssef-erremili <yousseferremili19@gmail.com>
2024-06-01 01:46:37 +0100

    Add job section

---
+++ job/listing.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Job Listings</title>
+    <link rel="stylesheet" href="../assets/styles/job.css">
+</head>
+<body>
+    <h1>Recent Job Circulars</h1>
+    <p class="subtitle">Many desktop publishing packages and web page editors</p>
+    <div id="loader" class="loader"></div>
+    <div id="jobsContainer" class="hidden"></div>
+
+
+    
+    <script src="../assets/js/job.js"></script>
+</body>
+</html>
