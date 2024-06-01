bcd28ceec8f9161813740a2d877767fdc8a375f1
youssef-erremili <yousseferremili19@gmail.com>
2024-06-01 19:57:43 +0100

    Fix links

--- template/template.php
+++ template/template.php
@@ -147,5 +147,4 @@ $stmt->execute();
     <script src="../assets/js/main.js"></script>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
 </body>
-
 </html>
